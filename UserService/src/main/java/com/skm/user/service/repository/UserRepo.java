7563bce8cbe2cecea6b971ac854370889bc2b55d
1Coder-Shivam <6868shivamkumarmaurya@gmail.com>
2023-07-31 09:38:20 +0530

    user

--- UserService/src/main/java/com/skm/user/service/repository/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.skm.user.service.repository;
-
-import com.skm.user.service.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, String> {
-
-}
