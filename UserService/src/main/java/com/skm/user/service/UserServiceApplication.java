403d196a9a4fb6ab8b00ca3e542d8d3b41651b63
1Coder-Shivam <6868shivamkumarmaurya@gmail.com>
2023-07-31 09:42:23 +0530

    user2

--- UserService/src/main/java/com/skm/user/service/UserServiceApplication.java
+++ UserService/src/main/java/com/skm/user/service/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.skm.user.userservice;
+package com.skm.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
 
 }
